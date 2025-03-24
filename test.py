8654a7971d9c51a3449bee51306f2462105f5def
adig2208 <adityagurnani@gmail.com>
2025-03-24 01:28:30 -0400

    Database implementation

--- test.py
+++ test.py
@@ -1,9 +1,36 @@
+# import socket
+# import struct
+
+# s = socket.socket()
+# s.connect(('localhost', 5000))
+
+# # Pack Write request: op='W', name='mykey', len='5'
+# req = struct.pack('!c31s8s', b'W', b'mykey', b'5')
+# # Send header + data
+# s.sendall(req + b'Hello')
+
+# # Receive response (always 40 bytes)
+# resp = s.recv(40)
+# status, name, length = struct.unpack('!c31s8s', resp)
+# print("Write Response Status:", status.decode())
+
+# s.close()
+
 import socket
 import struct
 
 s = socket.socket()
 s.connect(('localhost', 5000))
 
-req = struct.pack('!c31s8s', b'W', b'mykey', b'5')
+# Read request for key 'mykey'
+req = struct.pack('!c31s8s', b'D', b'mykey', b'0')
 s.sendall(req)
+
+# Read the 40-byte response
+resp = s.recv(40)
+status, name, length = struct.unpack('!c31s8s', resp)
+print("Read Response Status:", status.decode())
+
 s.close()
+
+
