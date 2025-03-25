505c17e3dd62b480f5b8368ec3e90f5c8c167041
adig2208 <adityagurnani@gmail.com>
2025-03-24 22:31:08 -0400

    multi-threaded-queue implementation

--- database.h
+++ database.h
@@ -1,7 +1,17 @@
 #ifndef DATABASE_H
 #define DATABASE_H
 
+struct db_record {
+    char record_name[31];
+    int status;
+};
+
+extern struct db_record db_table[200];
+
 int db_write(char *name, char *data, int len);
 int db_read(char *name, char *buf);
 int db_delete(char *name);
+int count_valid_objects();
+void db_cleanup(void);
+
 #endif
